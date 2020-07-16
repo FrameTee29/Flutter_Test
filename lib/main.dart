544d58ecdf8b65f679b8bc270a38f0a4e817e1fb
FrameTee29 <teeraphat123@hotmail.com>
2020-07-16 22:44:15 +0700

    Statelessful

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
-import 'package:flutter/material.dart' ; 
+import 'package:flutter/material.dart';
+import 'package:testapp/screens/home.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello'),
-
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
     );
   }
 }
