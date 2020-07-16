f0c074bf96daf75ed132366c2ad686852839755b
FrameTee29 <teeraphat123@hotmail.com>
2020-07-17 00:44:15 +0700

    button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -26,6 +26,40 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Widget signInbutton() {
+    return RaisedButton(
+      color: Colors.green[800],
+      child: Text(
+        'Sign in',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signupbutton() {
+    return OutlineButton(
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        signInbutton(),
+        SizedBox(
+          width: 8.0,
+        ),
+        signupbutton()
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +69,14 @@ class _HomeState extends State<Home> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             showlogo(),
+            SizedBox(
+              height: 8.0,
+            ),
             showAppName(),
+            SizedBox(
+              height: 8.0,
+            ),
+            showButton(),
           ],
         ),
       )),
