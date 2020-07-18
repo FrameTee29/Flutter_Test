cb7907f37b26dc8f8c56410a73ba4dc740af91bb
FrameTee29 <teeraphat123@hotmail.com>
2020-07-18 12:40:47 +0700

    Icon Upload

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -6,11 +6,28 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  // Explicit
+
+  // Method
+
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      onPressed: () {
+        print('You Click Upload');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.grey,
         title: Text("Register"),
+        actions: <Widget>[
+          registerButton(),
+        ],
       ),
       body: Text('body'),
     );
